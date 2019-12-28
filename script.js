e80a3a94980a8de42e4ec69d05c8c9ee85c0c990
chriskoder <gerokristof0909@gmail.com>
2019-12-28 18:01:34 +0100

    Added buttons, made the default design

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const container = document.querySelector('.container');
 const mainContainer = document.querySelector('.main-container');
-const boxWidth = 400;
-mainContainer.style.display = 'grid';
-mainContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
-container.style.gridColumnStart = '2';
-container.style.display = 'grid';
-container.style.gridTemplateRows = 'auto';
-container.style.width = '350px';
-container.style.border = '1px solid black';
-container.style.justifySelf = 'center';
-draw(16);
-
+const resizeButton = document.querySelector('.button1');
+const randomButton = document.querySelector('.button2');
+const clearButton = document.querySelector('.button3');
+const boxWidth = 400; //the width of the drawing area is 400px
+let randomBrush;
+let size, nsize;
+let init = (function() {
+    randomBrush = false;
+    const defSize = 16;
+    nsize = defSize;
+    mainContainer.style.display = 'grid';
+    mainContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
+    container.style.gridColumnStart = '2';
+    container.style.gridRowStart = '3';
+    container.style.display = 'grid';
+    container.style.gridTemplateRows = 'auto';
+    container.style.width = '350px';
+    container.style.border = '2px solid gray';
+    container.style.justifySelf = 'center';
+    randomButton.addEventListener('click', function() { 
+        if (randomBrush === false) {
+            randomBrush = true; 
+        } else {
+            randomBrush = false; 
+        }
+    });
+    resizeButton.addEventListener('click', resize);
+    clearButton.addEventListener('click', clear);
+    draw(defSize);
+})();
 function draw(n) {
-    let size = n;
+    size = n;
     container.style.gridTemplateColumns = 'repeat(' + size + ', 1fr)';
     container.style.gridAutoColumns = boxWidth/size + 'px';
     container.style.gridAutoRows = boxWidth/size + 'px';
@@ -20,10 +39,37 @@ function draw(n) {
         let element = document.createElement('div');
         element.textContent = '';
         element.onmouseenter = function() {
-            console.log('over');
-            element.style.background = 'black';
+            if (!element.classList.contains('colored')) {
+                if (randomBrush) {
+                    element.style.background = randomColor();
+                } else {
+                    element.style.background = 'white';
+                }
+                element.classList.add('colored');
+            }
         };
-    
         container.appendChild(element);
     }
+}
+function randomColor() {
+    let r, g, b;
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+    str = 'rgb(' + r + ', ' + g + ', ' + b + ')'
+    return str;
+}
+function resize() {
+    container.innerHTML = '';
+    nsize = prompt('What should be the new size of the drawing are? (E.g type "32" for 32x32)')
+    if (nsize > 0 && nsize < 129) {
+        draw(nsize);
+    } else {
+        alert('Please enter a num between 0 and 128');
+        resize();
+    }
+}
+function clear() {
+    container.innerHTML = '';
+    draw(nsize)
 }
